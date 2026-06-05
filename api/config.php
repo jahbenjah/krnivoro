9803b7334b8b7fd9235fc6c6b584fcc93cc72ca7
MauricioAlcala <a_mauricio@live.com>
2026-06-05 09:03:52 -0700

    DB

--- api/config.php
+++ api/config.php
@@ -3,9 +3,9 @@
 // Centraliza las credenciales de conexión a la base de datos
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'karnivor_krnivoro');
-define('DB_USER', 'karnivor_krnivoro'); 
-define('DB_PASS', 'karnivor_krnivoro01'); 
+define('DB_NAME', 'javie320_krnivoro');
+define('DB_USER', 'javie320_krnivoro'); 
+define('DB_PASS', 'javie320_Krn1v@r@.c@m'); 
 
 function getPDO() {
     return new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8mb4', DB_USER, DB_PASS);
